697541161b353540d5282c0d00b328cbbe9424f0
John Stray <github@johnstray.com>
2025-12-15 03:16:30 +0930

    Upgrades to the 'Theme Management' page. Laying groundwork for implementing better theme management. Part of #2, #3, #5

--- admin/template/sidebar-theme.php
+++ admin/template/sidebar-theme.php
@@ -1,49 +1,41 @@
 <?php
 
-if (!defined('IN_GS')) {
-    die('you cannot load this page directly.');
-}
-
 /**
- * Sidebar Themes Template
+ * Admin Template - Themes Sidebar
  *
- * @package GetSimple
+ * @package GetSimpleCMS
+ * @subpackage ThemeManagement
+ * @author John Stray <john.stray@getsimple.info>
  */
-?>
-<ul class="snav">
-    <li id="sb_theme" >
-        <a href="theme.php" <?php check_menu('theme');  ?>
-            accesskey="<?php echo find_accesskey(i18n_r('SIDE_CHOOSE_THEME'));?>" >
-            <?php i18n('SIDE_CHOOSE_THEME'); ?>
-        </a>
-    </li>
-    <li id="sb_themeedit" >
-        <a href="theme-edit.php" <?php check_menu('theme-edit'); ?>
-            accesskey="<?php echo find_accesskey(i18n_r('SIDE_EDIT_THEME'));?>" >
-            <?php i18n('SIDE_EDIT_THEME'); ?>
-        </a>
-    </li>
-    <li id="sb_components" >
-        <a href="components.php" <?php check_menu('components'); ?>
-            accesskey="<?php echo find_accesskey(i18n_r('SIDE_COMPONENTS'));?>" >
-            <?php i18n('SIDE_COMPONENTS'); ?>
-        </a>
-    </li>
-    <li id="sb_snippets" <?php if (getDef('GSNOSITEMAP', true)) {echo 'class="last_sb"';} ?>>
-        <a href="snippets.php" <?php check_menu('snippets'); ?>
-            accesskey="<?php echo find_accesskey(i18n_r('SIDE_SNIPPETS'));?>" >
-            <?php i18n('SIDE_SNIPPETS'); ?>
-        </a>
-    </li>
+
+if (defined('IN_GS') === false) {
+    die('You cannot load this file directly.');
+} ?>
+
+<nav class="nav flex-column">
+    <a id="sb_theme" class="nav-link<?php echo isPage('theme') ? ' active' : ''; ?>" href="theme.php"
+        accesskey="<?php echo find_accesskey(i18n_r('SIDE_CHOOSE_THEME')); ?>">
+        <?php i18n('SIDE_CHOOSE_THEME'); ?>
+    </a>
+    <a id="sb_themeedit" class="nav-link<?php echo isPage('theme-edit') ? ' active' : ''; ?>" href="theme-edit.php"
+        accesskey="<?php echo find_accesskey(i18n_r('SIDE_EDIT_THEME')); ?>">
+        <?php i18n('SIDE_EDIT_THEME'); ?>
+    </a>
+    <a id="sb_components" class="nav-link<?php echo isPage('components') ? ' active' : ''; ?>" href="components.php"
+        accesskey="<?php echo find_accesskey(i18n_r('SIDE_COMPONENTS')); ?>">
+        <?php i18n('SIDE_COMPONENTS'); ?>
+    </a>
+    <a id="sb_snippets" class="nav-link<?php echo isPage('snippets') ? ' active' : ''; ?>" href="snippets.php"
+        accesskey="<?php echo find_accesskey(i18n_r('SIDE_SNIPPETS')); ?>">
+        <?php i18n('SIDE_SNIPPETS'); ?>
+    </a>
     <?php if (!getDef('GSNOSITEMAP')) { ?>
-        <li id="sb_sitemap" class="last_sb">
-            <a href="sitemap.php" <?php check_menu('sitemap'); ?>
-                accesskey="<?php echo find_accesskey(i18n_r('SIDE_VIEW_SITEMAP'));?>" >
-                <?php i18n('SIDE_VIEW_SITEMAP'); ?>
-            </a>
-        </li>
+        <a id="sb_sitemap" class="nav-link<?php echo isPage('sitemap') ? ' active' : ''; ?>" href="sitemap.php"
+            accesskey="<?php echo find_accesskey(i18n_r('SIDE_VIEW_SITEMAP')); ?>">
+            <?php i18n('SIDE_VIEW_SITEMAP'); ?>
+        </a>
     <?php } ?>
     <?php exec_action("theme-sidebar"); // @hook theme-sidebar sidebar list html output  ?>
-</ul>
+</nav>
 
-<p id="js_submit_line" ></p>
+<p id="js_submit_line"></p>
--- admin/theme.php
+++ admin/theme.php
@@ -1,6 +1,11 @@
-<?php 
+<?php
+
+// phpcs:disable Generic.Files.LineLength.TooLong
+
 /**
- * Theme
+ * Themes
+ *
+ * Allows selection of site theme
  *
  * @package GetSimple
  * @subpackage Theme
@@ -14,87 +19,132 @@ login_cookie_check();
 exec_action('load-theme');
 
 # was the form submitted?
-if( (isset($_POST['submitted'])) && (isset($_POST['template'])) ) {
+if ((isset($_POST['submitted'])) && (isset($_POST['template']))) {
+    check_for_csrf("activate");
 
-	check_for_csrf("activate");	
-		
-	# get passed value from form
-	$newTemplate = var_in($_POST['template']);
+    # get passed value from form
+    $newTemplate = var_in($_POST['template']);
 
-	if(!path_is_safe(GSTHEMESPATH.$newTemplate,GSTHEMESPATH)) die();
+    if (!path_is_safe(GSTHEMESPATH . $newTemplate, GSTHEMESPATH)) {
+        die();
+    }
 
-	# backup old GSWEBSITEFILE (website.xml) file
-	backup_datafile(GSDATAOTHERPATH.GSWEBSITEFILE);
-	
-	# udpate GSWEBSITEFILE (website.xml) file with new theme
-	$xml = getXML(GSDATAOTHERPATH.GSWEBSITEFILE);
-	$xml->editAddCData('TEMPLATE',$newTemplate);
-	$status = XMLsave($xml,GSDATAOTHERPATH.GSWEBSITEFILE);
-	
-	$success = i18n_r('THEME_CHANGED');
+    # backup old GSWEBSITEFILE (website.xml) file
+    backup_datafile(GSDATAOTHERPATH . GSWEBSITEFILE);
 
-	$TEMPLATE = $newTemplate; // set new global
+    # udpate GSWEBSITEFILE (website.xml) file with new theme
+    $xml = getXML(GSDATAOTHERPATH . GSWEBSITEFILE);
+    $xml->editAddCData('TEMPLATE', $newTemplate);
+    $status = XMLsave($xml, GSDATAOTHERPATH . GSWEBSITEFILE);
+
+    $success = i18n_r('THEME_CHANGED');
+
+    $TEMPLATE = $newTemplate; // set new global
 }
 
 # get available themes, using folder match required contain template.php
-$themes = getDirs(GSTHEMESPATH,GSTEMPLATEFILE);
-$theme_options 	= '';
+$themes = getDirs(GSTHEMESPATH, GSTEMPLATEFILE);
+$theme_options = '';
 
-foreach($themes as $theme){
-	$sel       = $TEMPLATE == $theme ? 'selected' : '';
-	$themename = $TEMPLATE == $theme ? $theme. ' ('.i18n_r('ACTIVE').')' : $theme;
-	$theme_options .= '<option '.$sel.' value="'.$theme.'" >'.$themename.'</option>';
+foreach ($themes as $theme) {
+    $sel = $TEMPLATE == $theme ? 'selected' : '';
+    $themename = $TEMPLATE == $theme ? $theme . ' (' . i18n_r('ACTIVE') . ')' : $theme;
+    $theme_options .= '<option ' . $sel . ' value="' . $theme . '" >' . $themename . '</option>';
 }
 
 $pagetitle = i18n_r('THEME_MANAGEMENT');
 get_template('header');
 
 ?>
-	
+
 <?php include('template/include-nav.php'); ?>
 
-<div class="bodycontent clearfix">
-	
-	<div id="maincontent">
-		<div class="main">
-			<h3 class="floated"><?php i18n('CHOOSE_THEME');?></h3>
-			<div class="edit-nav clearfix" >
-				<?php exec_action(get_filename_id().'-edit-nav'); ?>
-			</div>		
-			<?php exec_action(get_filename_id().'-body'); ?>			
-		<form action="<?php myself(); ?>" method="post" accept-charset="utf-8" >
-				<input id="nonce" name="nonce" type="hidden" value="<?php echo get_nonce("activate"); ?>" />			
-			<?php	
-				$theme_path = str_replace(GSROOTPATH,'',GSTHEMESPATH);
-				if ( $SITEURL ) {	
-					echo '<p><b>'.i18n_r('THEME_PATH').': &nbsp;</b> <code>'.$SITEURL.$theme_path.$TEMPLATE.'/</code></p>';
-				}
-			?>
-				<p>
-					<select id="theme_select" class="text"  name="template" >
-					<?php echo $theme_options; ?>
-					</select>&nbsp;&nbsp;&nbsp;<input class="submit" type="submit" name="submitted" value="<?php i18n('ACTIVATE_THEME');?>" />
-				</p>
-			</form>
-			<?php
-			 	if (file_exists(GSTHEMESPATH.$TEMPLATE.'/images/screenshot.png')) { 
-					echo '<p><img id="theme_preview" src="../'.$theme_path.$TEMPLATE.'/images/screenshot.png" alt="'.i18n_r('THEME_SCREENSHOT').'" /></p>';
-					echo '<span id="theme_no_img" style="visibility:hidden"><p><em>'.i18n_r('NO_THEME_SCREENSHOT').'</em></p></span>';				
-				} else {
-					echo '<p><img id="theme_preview" style="visiblity:hidden;"" src="../'.$theme_path.$TEMPLATE.'/images/screenshot.png" alt="'.i18n_r('THEME_SCREENSHOT').'" /></p>';				
-					echo '<span id="theme_no_img"><p><em>'.i18n_r('NO_THEME_SCREENSHOT').'</em></p></span>';
-				}
+    <main id="maincontent" class="col-lg-9">
+        <div class="main">
+            <h3 class="floated"><?php i18n('CHOOSE_THEME');?></h3>
+            <div class="edit-nav clearfix" >
+                <?php exec_action(get_filename_id() . '-edit-nav'); ?>
+            </div>
 
-				exec_action('theme-extras'); //@hook theme-extras after theme html output
-			?>
-				
-		</div>
-	
-	</div>
-	
-	<div id="sidebar" >
-		<?php include('template/sidebar-theme.php'); ?>
-	</div>
+            <?php exec_action(get_filename_id() . '-body'); ?>
 
-</div>
+            <form action="<?php myself(); ?>" method="post" accept-charset="utf-8" >
+                <input id="nonce" name="nonce" type="hidden" value="<?php echo get_nonce("activate"); ?>" />
+                <div class="row g-3 align-items-center border-bottom pb-3 mb-5">
+                    <div class="col-auto">
+                        <label for="theme_select" class="col-form-label"><?php i18n('THEME_MANAGEMENT'); ?></label>
+                    </div>
+                    <div class="col">
+                        <select id="theme_select" class="form-select text"  name="template" ><?php echo $theme_options; ?></select>
+                    </div>
+                    <div class="col-auto">
+                        &nbsp;&nbsp;&nbsp;<input class="btn btn-default-outline submit" type="submit" name="submitted" value="<?php i18n('ACTIVATE_THEME'); ?>" />
+                    </div>
+                </div>
+            </form>
+
+            <section id="theme-details" class="row py-5 row-cols-1 row-cols-md-2 g-4 align-items-center">
+                <div class="col text-center">
+                    <div class="theme-preview w-75">
+                        <?php if (file_exists(GSTHEMESPATH . $TEMPLATE . '/images/screenshot.png')) { ?>
+                            <?php $theme_path = str_replace(GSROOTPATH, '', GSTHEMESPATH); ?>
+                            <img id="theme_preview" class="w-100" src="../<?php echo $theme_path . $TEMPLATE; ?>/images/screenshot.png" alt="<?php i18n('THEME_SCREENSHOT'); ?>" />
+                        <?php } else { ?>
+                            <div id="theme_no_img" class="border p-5">
+                                <p><em><?php i18n_r('NO_THEME_SCREENSHOT'); ?></em></p>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+                <div class="col">
+                    <h4>
+                        <?php echo $TEMPLATE; ?> Theme
+                        <small style="color:var(--primary-light);float:right;">v1.0.0</small>
+                    </h4>
+                    <p>
+                        <strong>Author:</strong> Theme Author Name<br />
+                        <strong>Website:</strong> <a href="https://example.com" target="_blank">https://example.com</a><br />
+                    </p>
+                    <p>
+                        This is a placeholder description for the <?php echo $TEMPLATE; ?> theme. Theme authors can provide information about the design, features, and intended use cases of their themes here.
+                    </p>
+                </div>
+            </section>
+
+            <!--<section id="theme-config" class="mt-5">
+                <h5>Theme Configuration</h5>
+                <p>
+                    Instructions for using and customizing the <?php echo $TEMPLATE; ?> theme will be provided here.
+                    This may include details on available templates, customization options, and any special features
+                    included with the theme.
+                </p>
+                <div class="row row-cols-1 row-cols-lg-2">
+                    <div class="col">
+                        <label for="exampleSetting" class="form-label">Example Setting</label>
+                        <p class="hint">This is an example setting for the theme. Adjust as needed.</p>
+                        <input type="text" class="form-control" id="exampleSetting" placeholder="Enter value">
+                    </div>
+                    <div class="col">
+                        <label for="anotherSetting" class="form-label">Another Setting</label>
+                        <p class="hint">This is an example setting for the theme. Adjust as needed.</p>
+                        <select class="form-select" id="anotherSetting">
+                            <option selected>Choose...</option>
+                            <option value="1">Option 1</option>
+                            <option value="2">Option 2</option>
+                            <option value="3">Option 3</option>
+                        </select>
+                    </div>
+                </div>
+            </section>-->
+
+            <?php exec_action('theme-extras'); //@hook theme-extras after theme html output ?>
+
+        </div>
+
+    </main>
+
+    <aside id="sidebar" class="col-lg-3">
+        <?php include('template/sidebar-theme.php'); ?>
+    </aside>
+
 <?php get_template('footer'); ?>
